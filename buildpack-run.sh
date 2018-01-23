7f0a4afe3ee4af872dae718fc81b04c8cbef6908
David Bower <dave@koales.co.uk>
2018-01-23 14:53:13 +0000

    Add shell and Python buildpacks and install AWS CLI tools in build process

---
+++ buildpack-run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#install aws-cli
+pip install awscli --upgrade --user
+# curl https://s3.amazonaws.com/aws-cli/awscli-bundle.zip -o awscli-bundle.zip
+# unzip awscli-bundle.zip
+# chmod +x ./awscli-bundle/install
+# ./awscli-bundle/install -i /tmp/aws
